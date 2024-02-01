1583c98aa909662def4369e587bdf6cedb787264
Thomas Tran <2001thomas.tran@gmail.com>
2024-02-01 16:40:45 -0600

    inquirer debugging

---
+++ db/queries.sql
@@ -0,0 +1,10 @@
+Table of employees with their titles, salaries, managers names
+SELECT employees.first_name, employees.last_name, title, salary, managers.first_name AS Manager_First_Name, managers.last_name AS Manager_Last_Name
+FROM employees 
+LEFT JOIN employees AS managers
+ON employees.manager_id = managers.id 
+JOIN role
+ON employees.role_id = role.id
+ORDER BY employees.id; 
+
+SELECT*FROM department
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // Import and require mysql2
 const mysql = require('mysql2');
-const inquirer = require('inquirer')
-const cTable = require('console.table')
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
+// const app = express();
 
 // Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -25,63 +26,68 @@ const db = mysql.createConnection(
 
 
 //Inquirer questions
-inquirer
+inquirer 
   .prompt([
     {
-        type: 'list',
-        message: '',
-        choies: [""],
-        name: ''
-    },
-    {
       type: 'list',
-      message: '',
-      choies: [""],
-      name: ''
-    },
-    {
-        type: 'list',
-        message: '',
-        choies: [""],
-        name: ''
-    },
-    {
-        type: 'list',
-        message: '',
-        choies: [""],
-        name: ''
-    },
-    {
+      message: 'What would you like to do?',
+      name: 'menu',
+      Choices: [
+      {
+        //**TODO view all departments
         type: 'input',
-        message: '',
-        name: ''
-    },
-    {
+        message: 'View all departments.',
+        name: 'Departments'
+      },
+      {
+        //**TODO view all roles
         type: 'input',
-        message: '',
-        name: ''
-    },
-    {
+        message: 'View all roles.',
+        name: 'Roles'
+      },
+      {
+        //**TODO view all employees
         type: 'input',
-        message: '',
-        name: ''
-    },
-    {
+        message: 'View all employees.',
+        name: 'employees'
+      },
+      {
+        //**TODO add a department
         type: 'input',
-        message: '',
+        message: 'Add a department.',
         name: ''
+      },
+      {
+        //**TODO add a role
+        type: 'input',
+        message: 'Add a role.',
+        name: ''
+      },
+      {
+        //**TODO add an employee
+        type: 'input',
+        message: 'Add an employee.',
+        name: ''
+      },
+      {
+        //**TODO update an employee role
+        type: 'input',
+        message: 'Update an employee role.',
+        name: ''
+      }]
     }
   ])
+  
   .then((response) => {
-    if (response.answer === "View all departments"){
-      db.query()
+    if (response.answer === "View all departments") {
+      db.query('SELECT*FROM department')
     }
   });
 
 app.use((req, res) => {
-    res.status(404).end();
-  });
+  res.status(404).end();
+});
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
