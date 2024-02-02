1bc3e2dbc45f8860366f35f0b5bc841e07349763
Thomas Tran <2001thomas.tran@gmail.com>
2024-02-01 20:36:52 -0600

    inquirer now asks a series of questions and each question will change the schemas.sql

--- db/queries.sql
+++ db/queries.sql
@@ -1,5 +1,7 @@
-Table of employees with their titles, salaries, managers names
-SELECT employees.first_name, employees.last_name, title, salary, managers.first_name AS Manager_First_Name, managers.last_name AS Manager_Last_Name
+-- *Table of employees with their titles, salaries, managers names
+SELECT employees.first_name, employees.last_name, title, salary, managers.first_name 
+AS Manager_First_Name, managers.last_name 
+AS Manager_Last_Name
 FROM employees 
 LEFT JOIN employees AS managers
 ON employees.manager_id = managers.id 
@@ -7,4 +9,5 @@ JOIN role
 ON employees.role_id = role.id
 ORDER BY employees.id; 
 
-SELECT*FROM department
+-- *View all departments
+SELECT*FROM department;
--- index.js
+++ index.js
@@ -4,20 +4,13 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 const { default: Choices } = require('inquirer/lib/objects/choices');
 
-const PORT = process.env.PORT || 3001;
-// const app = express();
-
-// Express middleware
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
 // Connect to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
     password: 'Copium',
-    database: 'books_db'
+    database: 'killmonger_db'
   },
   console.log(`Connected to the killmonger_db database.`)
 );
@@ -26,68 +19,182 @@ const db = mysql.createConnection(
 
 
 //Inquirer questions
-inquirer 
-  .prompt([
-    {
-      type: 'list',
-      message: 'What would you like to do?',
-      name: 'menu',
-      Choices: [
+function menuPrompt() {
+  inquirer
+    .prompt([
       {
-        //**TODO view all departments
-        type: 'input',
-        message: 'View all departments.',
-        name: 'Departments'
-      },
-      {
-        //**TODO view all roles
-        type: 'input',
-        message: 'View all roles.',
-        name: 'Roles'
-      },
-      {
-        //**TODO view all employees
-        type: 'input',
-        message: 'View all employees.',
-        name: 'employees'
-      },
-      {
-        //**TODO add a department
-        type: 'input',
-        message: 'Add a department.',
-        name: ''
-      },
-      {
-        //**TODO add a role
-        type: 'input',
-        message: 'Add a role.',
-        name: ''
-      },
-      {
-        //**TODO add an employee
-        type: 'input',
-        message: 'Add an employee.',
-        name: ''
-      },
-      {
-        //**TODO update an employee role
-        type: 'input',
-        message: 'Update an employee role.',
-        name: ''
-      }]
-    }
-  ])
-  
-  .then((response) => {
-    if (response.answer === "View all departments") {
-      db.query('SELECT*FROM department')
-    }
-  });
-
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'menu',
+        choices: ['View all departments.',
+          'View all roles.',
+          'View all employees.',
+          'Add a department.',
+          'Add a role.',
+          'Add an employee.',
+          'Update an employee role.',
+        ]
+      }
+    ])
+    .then((res) => {
+      if (res.menu === 'View all departments.') {
+        db.query('SELECT*FROM department ORDER BY department.name', (err, result) => {
+          if (err) {
+            statusMessage(400).console.log({ error: err.message });
+          } else {
+            console.table(result)
+            menuPrompt()
+          }
+        })
+      } else if (res.menu === 'View all roles.') {
+        db.query('SELECT*FROM role ORDER BY role.department_id', (err, result) => {
+          if (err) {
+            statusMessage(400).console.log({ error: err.message });
+          } else {
+            console.table(result)
+            menuPrompt()
+          }
+        })
+      } else if (res.menu === 'View all employees.') {
+        db.query(`SELECT employees.first_name, employees.last_name, title, salary, managers.first_name 
+        AS Manager_First_Name, managers.last_name 
+        AS Manager_Last_Name
+        FROM employees 
+        LEFT JOIN employees AS managers
+        ON employees.manager_id = managers.id 
+        JOIN role
+        ON employees.role_id = role.id
+        ORDER BY employees.id` , (err, result) => {
+          if (err) {
+            statusMessage(400).console.log({ error: err.message });
+          } else {
+            console.table(result)
+            menuPrompt()
+          }
+        })
+      } else if (res.menu === 'Add a department.') {
+        inquirer
+          .prompt([
+            {
+              type: 'input',
+              message: 'Name of the department?',
+              name: 'add_department'
+            }
+          ])
+          .then((res) => {
+            db.query('INSERT INTO department(name) VALUES (?)',
+              res.add_department, (err, result) => {
+                if (err) {
+                  statusMessage(400).console.log({ error: err.message });
+                } else {
+                  console.table(result)
+                  console.log(`Added ${res.add_department} as a department`);
+                  menuPrompt();
+                }
+              }
+            )
+          })
+      } else if (res.menu === 'Add a role.') {
+        db.query("SELECT * FROM department ORDER BY department.name",
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            const roleSwitched = result.map((role) => {
+              return {
+                name: role.name,
+                value: role.id,
+              };
+            });
+            inquirer
+              .prompt([
+                {
+                  type: 'input',
+                  message: 'Title of the role?',
+                  name: 'add_role'
+                },
+                {
+                  type: 'input',
+                  message: 'Salary of the role?',
+                  name: 'add_salary'
+                },
+                {
+                  type: 'list',
+                  message: 'Which department does this role go in?',
+                  name: 'add_to_department',
+                  choices: roleSwitched,
+                },
+              ])
+            .then((res) => {
+              db.query('INSERT INTO role(title, salary, department_id) VALUES (?,?,?)',
+                [res.add_role, res.add_salary, res.add_to_department], (err, result) => {
+                  if (err) {
+                    statusMessage(400).console.log({ error: err.message });
+                  } else {
+                    console.table(result)
+                    console.log(`Added ${res.add_role} as a role.`);
+                    menuPrompt();
+                  }
+                })
+            })
+          }
+        })
+      
+      } else if (res.menu === 'Add an employee.') {
+        db.query("SELECT * FROM department ORDER BY department.name",
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            const roleSwitched = result.map((role) => {
+              return {
+                name: role.name,
+                value: role.id,
+              };
+            });
+            inquirer
+              .prompt([
+                {
+                  type: 'input',
+                  message: 'Title of the role?',
+                  name: 'add_role'
+                },
+                {
+                  type: 'input',
+                  message: 'Salary of the role?',
+                  name: 'add_salary'
+                },
+                {
+                  type: 'list',
+                  message: 'Which department does this role go in?',
+                  name: 'add_to_department',
+                  choices: roleSwitched,
+                },
+              ])
+            .then((res) => {
+              db.query('INSERT INTO role(title, salary, department_id) VALUES (?,?,?)',
+                [res.add_role, res.add_salary, res.add_to_department], (err, result) => {
+                  if (err) {
+                    statusMessage(400).console.log({ error: err.message });
+                  } else {
+                    console.table(result)
+                    console.log(`Added ${res.add_role} as a role.`);
+                    menuPrompt();
+                  }
+                })
+            })
+          }
+        })
+      } else {
+        db.query('SELECT*FROM role ORDER BY role.department_id', (err, result) => {
+          if (err) {
+            statusMessage(400).console.log({ error: err.message });
+          } else {
+            console.table(result)
+            menuPrompt()
+          }
+        })
+      }
+    })
+}
+menuPrompt()
